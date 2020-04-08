ccacd6560d7e900c53b89f14a92833fedcc6bc88
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-04-08 15:06:59 +0300

    л

---
+++ platformer/Assets/water.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water : MonoBehaviour
+{
+    PlayerCont player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {
+            player = collision.gameObject.GetComponent<PlayerCont>();
+            player.inWater = true;
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player = collision.gameObject.GetComponent<PlayerCont>();
+            player.inWater = false;
+        }
+    }
+}
