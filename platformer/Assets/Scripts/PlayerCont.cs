b59d117363a292d93f2d700e4e01276b880d4462
PavelGem-13g <59193313+PavelGem-13g@users.noreply.github.com>
2020-04-08 15:06:47 +0300

    5

--- platformer/Assets/Scripts/PlayerCont.cs
+++ platformer/Assets/Scripts/PlayerCont.cs
@@ -4,37 +4,85 @@ using UnityEngine;
 
 public class PlayerCont : MonoBehaviour
 {
+Animator anim;
     public float speed;
     public float jumpForce;
-    public int health;
+    public int health=3;
     bool isDead = false;
     public int jumpCount;
+    public float timer = 3f;
+    bool dead = false;
+    public bool inWater;
+    public int oxygen;
     Rigidbody2D rb;
-    
+    CircleCollider2D cirCol;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cirCol = GetComponent<CircleCollider2D>();
+        anim= GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D)) 
+anim.SetBool("isRun",false);
+        //timer = Time.deltaTime;
+        /*if (timer <= 0)
         {
-            rb.AddForce(new Vector2(speed*Time.deltaTime,rb.velocity.y));
+            timer = 3f;
+        }*/
+        //if(inWater==TransparencySortMode)
+        cirCol = GetComponent<CircleCollider2D>();
+        if (inWater == true) 
+        {
+            oxygen--;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+            //rb.AddForce(new Vector2(speed * Time.deltaTime, rb.velocity.y));
+            rb.velocity = new Vector2(speed * Time.deltaTime, rb.velocity.y);
+
+anim.SetBool("isRun",true);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(new Vector2(-speed * Time.deltaTime, rb.velocity.y));
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            //rb.AddForce(new Vector2(-speed * Time.deltaTime, rb.velocity.y));
+            rb.velocity = new Vector2(-speed * Time.deltaTime, rb.velocity.y);
+
+anim.SetBool("isRun",true);
+
         }
-        if (Input.GetKeyDown(KeyCode.Space)&& jumpCount>0) 
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
         {
             rb.velocity = Vector3.zero;
-            rb.AddForce(new Vector2(0, speed *jumpForce),ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(0, speed * jumpForce*15), ForceMode2D.Impulse);
+
+anim.SetBool("isRun",true);
             jumpCount--;
         }
-        Debug.Log(rb.velocity);
+        if (gameObject.transform.position.y < -10)
+        {
+            dead = true;
+            /*transform.position = new Vector3(0, 0, 0);
+            rb.velocity = Vector3.zero;*/
+        }
+        if (dead == true)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0 && health >= 1)
+            {
+                transform.position = Vector3.zero;
+                rb.velocity = Vector3.zero;
+                timer = 2f;
+                dead = false;
+                health--;
+            }
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -42,10 +90,30 @@ public class PlayerCont : MonoBehaviour
         {
             jumpCount = 2;
         }
-        if (collision.gameObject.tag == "DieLine") 
-        {
-            transform.position=new Vector3(0,0,0);   
-        }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Spike")
+        {
+            health--;
+            rb.AddForce(new Vector2(-200, 500));
+        }
+        if (health <= 0) 
+        {
+            rb.AddForce(new Vector2(0, 1000));
+            Destroy(cirCol);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Water") 
+        {
+            oxygen--;
+        }
+    }
 }
---
+++ platformer/Assets/score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class score : MonoBehaviour
+{
+    int Score;
+    int ScoreNow;
+    TextMeshProUGUI scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("Score", 0);
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreNow = PlayerPrefs.GetInt("Score");
+        scoreText.text = "Score -" + ScoreNow;
+        
+    }
+    public void ScoreUp() 
+    {
+        Score = PlayerPrefs.GetInt("Score");
+        Score += 10;
+        PlayerPrefs.SetInt("Score",Score);
+    }
+}
